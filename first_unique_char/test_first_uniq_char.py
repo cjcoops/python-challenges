87727da085f8b2215806576b8322633c5d88c711
Chris Cooper <cjcooper252@gmail.com>
2024-10-18 12:02:47 -0700

    first_uniq_char

---
+++ first_unique_char/test_first_uniq_char.py
@@ -0,0 +1,21 @@
+from main import first_uniq_char
+
+
+def test_example1():
+    assert first_uniq_char("leetcode") == 0
+
+
+def test_example2():
+    assert first_uniq_char("loveleetcode") == 2
+
+
+def test_no_unique_char():
+    assert first_uniq_char("aabbcc") == -1
+
+
+def test_empty_string():
+    assert first_uniq_char("") == -1
+
+
+def test_single_char():
+    assert first_uniq_char("z") == 0
